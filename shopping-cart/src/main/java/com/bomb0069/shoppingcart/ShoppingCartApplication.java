77a2ca552a3989eaadd1aa22d35f0d4dca099097
karan sivarat <bomb0069@gmail.com>
2021-10-03 22:17:46 +0700

    [Added]: Shopping-cart Project

---
+++ shopping-cart/src/main/java/com/bomb0069/shoppingcart/ShoppingCartApplication.java
@@ -0,0 +1,13 @@
+package com.bomb0069.shoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartApplication.class, args);
+	}
+
+}
